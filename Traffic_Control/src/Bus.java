2ad950663805f02defb36c68986637f415be5da6
Constantinos Hasikos <hasikosc@gmail.com>
2016-01-28 12:37:57 +0000

    CH: Commiting the initial project

---
+++ Traffic_Control/src/Bus.java
@@ -0,0 +1,30 @@
+
+public class Bus implements Vehicle {
+
+	@Override
+	public int getPriority() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void sePriority(int priority) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPerson() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPerson() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
---
+++ Traffic_Control/src/Terrain.java
@@ -0,0 +1,8 @@
+
+public interface Terrain {
+
+	public void setInWhichNodeLocated();
+	public int getInWhichNodeLocated();
+	public int getPerson();
+	public void setPerson();
+}
