bc53a4391ed7fa4314395c23ea5bf751a3062842
toxicos <grigorispavl@gmail.com>
2016-02-07 21:33:44 +0200

    Update Node.java

--- Traffic_Control/src/Objects/Node.java
+++ Traffic_Control/src/Objects/Node.java
@@ -2,20 +2,18 @@ import java.util.ArrayList;
 
 public class Node {
 
-	private ArrayList<Object> myStack = new ArrayList();
-	
-	public Node() {
-		
-	}
-	
-	
-	public void getPreviousNode(){
-	
-	}
+	private ArrayList<Object> myStack = new ArrayList<Object>();
+	private String nameOfNode;
+	private double weightOfDifficulty;
+	private double time;
 
-	public void getNextNode(){
+	public Node(String text, double text2, double text3) {
 		
-	}	
+		this.setNameOfNode(text);
+		this.setWeightOfDifficulty(text2);
+		this.setTime(text3);
+
+	}
 	
 	public void addToStack(Object obj){
 		this.myStack.add(obj);
@@ -23,6 +21,30 @@ public class Node {
 
 	public void removeFromStack(Object obj){
 		this.myStack.remove(obj);
+	}
+
+	public String getNameOfNode() {
+		return nameOfNode;
+	}
+
+	public void setNameOfNode(String nameOfNode) {
+		this.nameOfNode = nameOfNode;
+	}
+
+	public double getWeightOfDifficulty() {
+		return weightOfDifficulty;
+	}
+
+	public void setWeightOfDifficulty(double weightOfDifficulty) {
+		this.weightOfDifficulty = weightOfDifficulty;
+	}
+
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(double time) {
+		this.time = time;
 	}
 
 }
