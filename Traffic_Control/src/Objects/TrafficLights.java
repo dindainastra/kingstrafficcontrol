2d9f7cf1b412ea1021757261d0bdd0c7600e06f9
Constantinos Hasikos <hasikosc@gmail.com>
2016-02-18 19:46:27 +0000

    This is the first version of the traffic lights. At the moment the logic is wrong (the sequence of the lights), im gonna fix it asap. I haven't figured out yet how im gonna use threads. I think one thread is running at the moment. I will try to implement more. The first light initialised is red although it doesnt come up first on the console window. Im gonna fix that as well.

--- Traffic_Control/src/Objects/TrafficLights.java
+++ Traffic_Control/src/Objects/TrafficLights.java
@@ -1,3 +1,4 @@
+package objects;
 
 public class TrafficLights implements Terrain{
 
@@ -90,25 +91,21 @@ public class TrafficLights implements Terrain{
     }
 
 
-    @Override
 	public void setInWhichNodeLocated() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public int getInWhichNodeLocated() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public int getPerson() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public void setPerson() {
 		// TODO Auto-generated method stub
 		
