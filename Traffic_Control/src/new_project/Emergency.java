a5d4b001f9c2bbe398569a0605fc6ab1b1956143
Daniella2015 <daniella.bacud@gmail.com>
2016-02-23 21:05:59 +0000

    DB:  created emergency cars

--- Traffic_Control/src/new_project/Emergency.java
+++ Traffic_Control/src/new_project/Emergency.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package new_project;
+import java.awt.*;
 
-/**
- *
- * @author daniella
- */
 public class Emergency {
+    // Variables declaration
+    private final int pos_x,pos_y;
+    private final int R = 255, G = 0, B=0; //Red
+    private final int length = 20 ,width = 15;
     
+    //set car position
+       Emergency(int x_coordinate, int y_coordinate){
+            this.pos_x = x_coordinate;
+            this.pos_y = y_coordinate;
+       }
+       
+    //draw Vehicle
+    protected void doDrawing(Graphics g){
+        g.setColor(new Color (R,G,B));
+        g.fillRect(pos_x, pos_y, length, width);   
+    }
+      
 }
