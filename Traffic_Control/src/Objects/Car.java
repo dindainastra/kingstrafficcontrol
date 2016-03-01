b417d846a22d3cab1b87fb7ff3d6aa218b6c39c2
saikurdukar <saitwentynine@yahoo.co.in>
2016-03-01 01:20:39 +0000

    Made changes to Draw.java and Car.java file.Got the car running.

--- Traffic_Control/src/Objects/Car.java
+++ Traffic_Control/src/Objects/Car.java
@@ -3,13 +3,19 @@ package Objects;
 //test 
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.Timer;
+
+
 public class Car implements Vehicle {
         
         // Variables declaration
 	private Person driver;
 	private int priorityLevel;
 	private int nodeID;
-        private final int pos_x,pos_y;
+        private int pos_x,pos_y;
         private final int R = 173, G = 216, B=230; //pastel blue
         private final int length = 20 ,width = 15;
     
@@ -26,7 +32,29 @@ public class Car implements Vehicle {
                 this.pos_x = x_coordinate;
                 this.pos_y = y_coordinate;
         }
+        
+        
 	
+        public int getpos_x(){
+            return this.pos_x;      
+            
+        }
+        
+        public int getpos_y(){
+            return this.pos_y;      
+            
+        }
+        
+        public void setpos_x(int x){
+            this.pos_x=x;      
+            
+        }
+        
+        public void setpos_y(int y){
+            this.pos_y=y;      
+            
+        }
+        
 	public int getPriority() {
 		return this.priorityLevel;
 	}
