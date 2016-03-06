78da57b25b10fb8a78d637e36de2084fae589215
saikurdukar <saitwentynine@yahoo.co.in>
2016-03-06 23:20:17 +0000

    Changed map size

--- Traffic_Control/src/Controllers/NodeManager.java
+++ Traffic_Control/src/Controllers/NodeManager.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import Objects.Bus;
 import Objects.Car;
 import Objects.Draw;
-import Objects.Junction;
+import Objects.SquareJunction;
 import Objects.Person;
 import Objects.Terrain;
 import Objects.TrafficLights;
