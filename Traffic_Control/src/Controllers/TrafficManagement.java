31112ef564457882ce6bc1e70d7d803215715a30
saikurdukar <saitwentynine@yahoo.co.in>
2016-03-28 13:57:27 +0100

    Changed the x coordinates by 100

--- Traffic_Control/src/Controllers/TrafficManagement.java
+++ Traffic_Control/src/Controllers/TrafficManagement.java
@@ -205,103 +205,66 @@ public class TrafficManagement extends JFrame {
     public void initializeStaticTrafficLights(){
 
         //junction25
-        //TrafficLights ninthTL = new TrafficLights(715,10,3,2,0,getTlDelay());
-        TrafficLights ninthTL = new TrafficLights(715,10,3,2,0,3000);
-        TrafficLights twelfthTL = new TrafficLights(815,60,3,1,0,3000);
-        TrafficLights eighteenthTL = new TrafficLights(765,110,3,1,90,3000);
+//        TrafficLights ninthTL = new TrafficLights(715,10,3,1,0,getTlDelay());
+//        TrafficLights twelfthTL = new TrafficLights(815,60,3,9,0,getTlDelay());
+//        TrafficLights eighteenthTL = new TrafficLights(765,110,3,5,90,getTlDelay());
+        TrafficLights ninthTL = new TrafficLights(815,10,3,1,0,getTlDelay());
+        TrafficLights twelfthTL = new TrafficLights(915,60,3,9,0,getTlDelay());
+        TrafficLights eighteenthTL = new TrafficLights(865,110,3,5,90,getTlDelay());
 
-        ninthTL.previousTrafficLightIs(eighteenthTL);
-        twelfthTL.previousTrafficLightIs(ninthTL);
-        eighteenthTL.previousTrafficLightIs(twelfthTL);
-        ninthTL.nextTrafficLightIs(twelfthTL);
-        twelfthTL.nextTrafficLightIs(eighteenthTL);
-        eighteenthTL.nextTrafficLightIs(ninthTL);
+        //junction 23
+        TrafficLights thirdTL = new TrafficLights(250,325,4, 1,90,getTlDelay());
+        TrafficLights firstTL = new TrafficLights(250,375,4, 13,0,getTlDelay());
+        TrafficLights fourthTL = new TrafficLights(200,425, 4, 9,90,getTlDelay());
+        TrafficLights secondTL = new TrafficLights(150,325,4, 5,0,getTlDelay());
 
-
-//        //junction 23
-        TrafficLights thirdTL = new TrafficLights(150,325,4, 2,90,3000);
-        TrafficLights firstTL = new TrafficLights(150,375,4, 1,0,3000);
-        TrafficLights fourthTL = new TrafficLights(100,425, 4, 1,90,3000);
-        TrafficLights secondTL = new TrafficLights(50,325,4, 1,0,3000);
-        thirdTL.previousTrafficLightIs(secondTL);
-        firstTL.previousTrafficLightIs(thirdTL);
-        fourthTL.previousTrafficLightIs(firstTL);
-        secondTL.previousTrafficLightIs(fourthTL);
-        thirdTL.nextTrafficLightIs(firstTL);
-        firstTL.nextTrafficLightIs(fourthTL);
-        fourthTL.nextTrafficLightIs(secondTL);
-        secondTL.nextTrafficLightIs(thirdTL);
+        //roundabout
+        TrafficLights fifteenTL = new TrafficLights(915,260,4, 1,90,getTlDelay());
+        TrafficLights eighthTL = new TrafficLights(980,375,4, 13,0,getTlDelay());
+        TrafficLights sixteenthTL = new TrafficLights(865,490,4 ,9,90,getTlDelay());
+        TrafficLights fifthTL = new TrafficLights(750,325,4, 5,0,getTlDelay());
 
 
-//        //roundabout
-        TrafficLights fifteenTL = new TrafficLights(815,260,4, 2,90,3000);
-        TrafficLights eighthTL = new TrafficLights(880,375,4, 1,0,3000);
-        TrafficLights sixteenthTL = new TrafficLights(765,490,4 ,1,90,3000);
-        TrafficLights fifthTL = new TrafficLights(650,325,4, 1,0,3000);
-        fifteenTL.previousTrafficLightIs(fifthTL);
-        eighthTL.previousTrafficLightIs(fifteenTL);
-        sixteenthTL.previousTrafficLightIs(eighthTL);
-        fifthTL.previousTrafficLightIs(sixteenthTL);
-        fifteenTL.nextTrafficLightIs(eighthTL);
-        eighthTL.nextTrafficLightIs(sixteenthTL);
-        sixteenthTL.nextTrafficLightIs(fifthTL);
-        fifthTL.nextTrafficLightIs(fifteenTL);
-//
-//
-//        //junction22
-        TrafficLights thirteenthTL = new TrafficLights(1180,325,4, 2,90,3000);
-        TrafficLights seventhTL = new TrafficLights(1180,375,4, 1,0,3000);
-        TrafficLights fourteenthTL = new TrafficLights(1130,425,4,1,90,3000);
-        TrafficLights sixthTL = new TrafficLights(1080,325,4,1,0,3000);
-        thirteenthTL.previousTrafficLightIs(sixthTL);
-        seventhTL.previousTrafficLightIs(thirteenthTL);
-        fourteenthTL.previousTrafficLightIs(seventhTL);
-        sixthTL.previousTrafficLightIs(fourteenthTL);
-        thirteenthTL.nextTrafficLightIs(seventhTL);
-        seventhTL.nextTrafficLightIs(fourteenthTL);
-        fourteenthTL.nextTrafficLightIs(sixthTL);
-        sixthTL.nextTrafficLightIs(thirteenthTL);
-//
-//
-//        //junction24
+        //junction22
+        TrafficLights thirteenthTL = new TrafficLights(1280,325,4, 1,90,getTlDelay());
+        TrafficLights seventhTL = new TrafficLights(1280,375,4, 13,0,getTlDelay());
+        TrafficLights fourteenthTL = new TrafficLights(1230,425,4,9,90,getTlDelay());
+        TrafficLights sixthTL = new TrafficLights(1180,325,4,5,0,getTlDelay());
 
-        TrafficLights seventeenthTL = new TrafficLights(815,590,3,2,90,3000);
-        TrafficLights eleventhTL = new TrafficLights(815,640,3,1,0,3000);
-        TrafficLights tenthTL = new TrafficLights(715,590,3,1,0,3000);
-        seventeenthTL.previousTrafficLightIs(tenthTL);
-        eleventhTL.previousTrafficLightIs(seventeenthTL);
-        tenthTL.previousTrafficLightIs(eleventhTL);
-        seventeenthTL.nextTrafficLightIs(eleventhTL);
-        eleventhTL.nextTrafficLightIs(tenthTL);
-        tenthTL.nextTrafficLightIs(seventeenthTL);
+
+        //junction24
+
+        TrafficLights seventeenthTL = new TrafficLights(915,590,3,1,90,getTlDelay());
+        TrafficLights eleventhTL = new TrafficLights(915,640,3,9,0,getTlDelay());
+        TrafficLights tenthTL = new TrafficLights(815,590,3,5,0,getTlDelay());
 
         // direction ---->
 
         //junction25
         aTerrainList.get(8).setForwardListFlow(ninthTL);
-        aTerrainList.get(11).setBackwardListFlow(twelfthTL); //supposed to be backward
-        aTerrainList.get(12).setBackwardListFlow(eighteenthTL);//up supposed to be backward
-//
-//        //junction23
+        aTerrainList.get(11).setForwardListFlow(twelfthTL); //supposed to be backward
+        aTerrainList.get(12).setForwardListFlow(eighteenthTL);//up supposed to be backward
+
+        //junction23
         aTerrainList.get(14).setForwardListFlow(thirdTL); //down supposed to be backward
         aTerrainList.get(6).setBackwardListFlow(firstTL); //supposed to be backward
-        aTerrainList.get(15).setBackwardListFlow(fourthTL); //up
+        aTerrainList.get(15).setForwardListFlow(fourthTL);
         aTerrainList.get(0).setForwardListFlow(secondTL);
-//
-//        //roundabout
+
+        //roundabout
         aTerrainList.get(12).setForwardListFlow(fifteenTL);//down
         aTerrainList.get(7).setBackwardListFlow(eighthTL); //supposed to be backward
-        aTerrainList.get(13).setBackwardListFlow(sixteenthTL);//up
+        aTerrainList.get(13).setForwardListFlow(sixteenthTL);//up
         aTerrainList.get(6).setForwardListFlow(fifthTL);
 
         //junction22
         aTerrainList.get(7).setForwardListFlow(sixthTL);
-        aTerrainList.get(1).setBackwardListFlow(seventhTL); //
+        aTerrainList.get(1).setForwardListFlow(seventhTL);
         aTerrainList.get(17).setForwardListFlow(thirteenthTL); //down
-        aTerrainList.get(16).setBackwardListFlow(fourteenthTL); //up
-//
+        aTerrainList.get(16).setForwardListFlow(fourteenthTL); //up
+
         //junction24
-        aTerrainList.get(13).setForwardListFlow(seventeenthTL);//down
+        aTerrainList.get(13).setBackwardListFlow(seventeenthTL);//down
         aTerrainList.get(10).setBackwardListFlow(eleventhTL);
         aTerrainList.get(9).setForwardListFlow(tenthTL);
 
@@ -401,46 +364,51 @@ public class TrafficManagement extends JFrame {
     public void staticMapCreator(){
 
         //create FIRSTLY the entry and exit roads
-        aTerrainList.add(new StraightRoad(10,325,0,40,this));//entry road    //0
-        aTerrainList.add(new StraightRoad(1180,325,0,170,this));//exit road  //1
+        //aTerrainList.add(new StraightRoad(10,325,0,40,this));//entry road    //0
+        //aTerrainList.add(new StraightRoad(1180,325,0,170,this));//exit road  //1
+        aTerrainList.add(new StraightRoad(50,325,0,100,this));//entry road    //0
+        aTerrainList.add(new StraightRoad(1280,325,0,100,this));//exit road  //1
 
         //next, create the rest map
 
         //add roundabout
-        aTerrainList.add(new CornerRoad(640,250,90,1,this));                         //2
-        aTerrainList.add(new CornerRoad(640,250,180,1,this));                        //3
-        aTerrainList.add(new CornerRoad(640,250,270,1,this));                        //4
-        aTerrainList.add(new CornerRoad(640,250,360,1,this));                        //5
+        aTerrainList.add(new CornerRoad(740,250,90,1,this));                         //2
+        aTerrainList.add(new CornerRoad(740,250,180,1,this));                        //3
+        aTerrainList.add(new CornerRoad(740,250,270,1,this));                        //4
+        aTerrainList.add(new CornerRoad(740,250,360,1,this));                        //5
 
         //add horizontal road
         //Added another traffic light color parameter to SRoad
-        aTerrainList.add(new StraightRoad(150,325,0,500,this));               //6   <-----
-        aTerrainList.add(new StraightRoad(880,325,0,200,this));
-        aTerrainList.add(new StraightRoad(150,10,0,565,this));
-        aTerrainList.add(new StraightRoad(150,590,0,565,this));
-        aTerrainList.add(new StraightRoad(815,590,0,265,this));
-        aTerrainList.add(new StraightRoad(815,10,0,265,this));
+        aTerrainList.add(new StraightRoad(250,325,0,500,this));               //6   <-----
+        aTerrainList.add(new StraightRoad(980,325,0,200,this));
+        aTerrainList.add(new StraightRoad(250,10,0,565,this));
+        aTerrainList.add(new StraightRoad(250,590,0,565,this));
+        aTerrainList.add(new StraightRoad(915,590,0,265,this));
+        aTerrainList.add(new StraightRoad(915,10,0,265,this));
 
         //add vertical roads
-        aTerrainList.add(new StraightRoad(715,110,90,150,this));              //12
-        aTerrainList.add(new StraightRoad(715,490,90,101,this));
-        aTerrainList.add(new StraightRoad(50,110,90,215,this));              //14
-        aTerrainList.add(new StraightRoad(50,425,90,165,this));
-        aTerrainList.add(new StraightRoad(1080,425,90,165,this));
-        aTerrainList.add(new StraightRoad(1080,110,90,215,this));
+        aTerrainList.add(new StraightRoad(815,110,90,150,this));              //12
+        aTerrainList.add(new StraightRoad(815,490,90,101,this));
+        aTerrainList.add(new StraightRoad(150,110,90,215,this));              //14
+        aTerrainList.add(new StraightRoad(150,425,90,165,this));
+        aTerrainList.add(new StraightRoad(1180,425,90,165,this));
+        aTerrainList.add(new StraightRoad(1180,110,90,215,this));
 
         //add curved roads
-        aTerrainList.add(new CornerRoad(980,10,360,0,this));                         //18
-        aTerrainList.add(new CornerRoad(980,490,270,0,this));                        //19
-        aTerrainList.add(new CornerRoad(50,10,90,0,this));                           //20
-        aTerrainList.add(new CornerRoad(50,490,180,0,this));                         //21
+        aTerrainList.add(new CornerRoad(1080,10,360,0,this));                         //18
+        aTerrainList.add(new CornerRoad(1080,490,270,0,this));                        //19
+        aTerrainList.add(new CornerRoad(150,10,90,0,this));                           //20
+        aTerrainList.add(new CornerRoad(150,490,180,0,this));                         //21
         //aTerrainList.add(new CornerRoad(300,75,90));
 
         //add junctions (0,0,4,3: to close the square junctions)
-        aTerrainList.add(new SquareJunction(1080,325,this,0));
-        aTerrainList.add(new SquareJunction(50,325,this,0));                           //23
-        aTerrainList.add(new SquareJunction(715,590,this,4));
-        aTerrainList.add(new SquareJunction(715,10,this,3));
+        aTerrainList.add(new SquareJunction(1180,325,this,0));
+        aTerrainList.add(new SquareJunction(150,325,this,0));                          //23
+        aTerrainList.add(new SquareJunction(815,590,this,4));
+        aTerrainList.add(new SquareJunction(815,10,this,3));
+//        aTerrainList.add(new SquareJunction(50,325,this,0));                          //23
+//        aTerrainList.add(new SquareJunction(715,590,this,4));
+//        aTerrainList.add(new SquareJunction(715,10,this,3));
 
     }
 
@@ -451,10 +419,12 @@ public class TrafficManagement extends JFrame {
     public void drawTheMap(final Draw aDraw){
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                frame = new JFrame("King's Traffic Control Simulation");
+                frame = new JFrame();
                 frame.setLayout(new BorderLayout());
+
                 frame.add(new Buttons(), BorderLayout.PAGE_END);
                 frame.add(new Slider(TrafficManagement.this), BorderLayout.EAST);
+
                 //frame.add(new Buttons(), BorderLayout.PAGE_END);
                 Buttons buttons=new Buttons();
                 buttons.setPreferredSize(new Dimension(100,50));
@@ -468,7 +438,6 @@ public class TrafficManagement extends JFrame {
                 frame.setVisible(true);
                 /*frame.setSize(1500, 1000);*/
                 frame.setSize(1400, 700);
-                frame.setLocationRelativeTo(null);
                 frame.setResizable ( false );
             }
         });
@@ -499,25 +468,11 @@ public class TrafficManagement extends JFrame {
     public void setTlDelay(int tlDelay) {
         this.tlDelay = tlDelay;
     }
-   public int getSliderPersons() {
-       return sliderPersons;
-   }
+    public int getSliderPersons() {
+        return sliderPersons;
+    }
 
     public void setSliderPersons(int sliderPersons) {
         this.sliderPersons = sliderPersons;
-    }
-
-    /**
-     *
-     */
-    public void initializeRoadworksTo(){
-        //the param should be the node
-
-        //choose which road that we want to apply the roadworks
-        // if it is not ( a junction || entrance || road)
-        // if (straightroad )
-        //if it has neighbour: corner road
-        // add the corner road and its neighbour too
-        // delete all of this
     }
 }
